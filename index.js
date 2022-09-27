d6ef2ed0605ce562e0802dad97f61a7088c4851f
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2022-09-27 18:54:58 +0300

    Working on week 3 assignments

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const { application } = require('express')
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
+const cors = require('cors')
+
 app.use(express.json())
+app.use(cors())
+
+morgan.token("data", (request) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : " ";
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let data = [
     { 
@@ -68,7 +79,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT ||3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
