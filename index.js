3003403cb7261ac8f5aef125b92736debf40526d
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2022-10-04 14:07:03 +0300

    Exercises 3.15-3.18

--- index.js
+++ index.js
@@ -94,6 +94,39 @@ app.post('/api/persons', (request, response) => {
     .catch((error) => next(error))
 })
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" })
+};
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+};
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
