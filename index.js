301f3451a0287f992e27490b3ff0b2c9d9e58f09
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2022-10-04 13:03:48 +0300

    Modified frontend and copied new build folder

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+
 
 morgan.token("data", (request) => {
   return request.method === "POST" ? JSON.stringify(request.body) : " ";
