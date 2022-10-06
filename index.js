1d04ec18bfc33386f1e8cb90db2b0c4da35b1cbe
samuelashraff <48100109+samuelashraff@users.noreply.github.com>
2022-10-06 16:24:55 +0300

    Moved dotenv require to top of the file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-require('dotenv').config()
 
 app.use(express.json())
 app.use(cors())
@@ -17,29 +17,6 @@ morgan.token("data", (request) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
-
-let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then((persons) => {
